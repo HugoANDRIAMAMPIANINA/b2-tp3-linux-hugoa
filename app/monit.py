dd7e07fe22701ffaf2a7dbab2e8111832b7fcdd6
ahugo <hugo.andriamampianina@ynov.com>
2024-01-11 11:43:43 +0100

    update

--- app/monit.py
+++ app/monit.py
@@ -12,9 +12,8 @@ def conf_file_exists():
     return False
 
 def create_config_file():
-    open("/etc/monit/monit.conf", "w")
     conf_json = { "tcp_ports": [] }
-    with open("/etc/monit/monit.conf", "w") as conf_file:
+    with open("/etc/monit/monit.conf", "x") as conf_file:
         dump(conf_json, conf_file)
 
 def get_ram_informations():
