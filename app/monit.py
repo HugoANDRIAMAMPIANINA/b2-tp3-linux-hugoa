7c93908f18287832ade080d42fef2e06c3c9dfbb
ahugo <hugo.andriamampianina@ynov.com>
2024-01-11 11:06:06 +0100

    update

--- app/monit.py
+++ app/monit.py
@@ -28,7 +28,7 @@ def get_cpu_usage():
     return cpu_percent()
 
 def check_tcp_ports():
-    print(net_connections())
+    return 1111 in [i.laddr.port for i in net_connections()]
     
 
 def check_system():
