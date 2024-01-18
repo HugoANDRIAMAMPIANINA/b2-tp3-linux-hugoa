a56274af527906b25d29221e8100b8979aaf4d46
ahugo <hugo.andriamampianina@ynov.com>
2024-01-18 12:14:50 +0100

    update

--- app/monit.py
+++ app/monit.py
@@ -28,7 +28,7 @@ def get_logger():
     
     log_file_path = '/var/log/monit/monit.log'
     file_handler = logging.FileHandler(log_file_path)
-    file_handler.setFormatter('%(asctime)s %(levelname)s %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+    file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s %(message)s', datefmt='%Y-%m-%d %H:%M:%S'))
 
     logger.addHandler(file_handler)
     return logger
